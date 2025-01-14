1105566d5d5bf75c14e37a4742ef7d53a9e32b12
Jakob <jtilsch@pvpc.org>
2025-01-14 15:25:27 -0500

    Initial commit for Census_Housing.py pipeline

---
+++ Census_Housing.py
@@ -0,0 +1,201 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import numpy as np
+import re
+import pyodbc
+import time
+
+# Write a function that data for each url does all the conversions and returns a dataframe
+def API_Data_Collector(link):
+    request = requests.get(link)
+    data = request.json()
+    df = pd.DataFrame(data[1:], columns = data[0], )
+    return df
+
+def Table_Concatenator(year, table):
+    URL = "https://api.census.gov/data/" + str(year) + "/acs/acs5?get=group(" + table + ")&for=county%20subdivision:*&in=state:25%20county:011,013,015"
+    URL_Counties = "https://api.census.gov/data/" + str(year) + "/acs/acs5?get=group(" + table + ")&for=county:011,013,015&in=state:25"
+    URL_State = "https://api.census.gov/data/" + str(year) + "/acs/acs5?get=group(" + table + ")&for=state:25"
+    links = [URL,URL_Counties,URL_State]
+    compiled_tables = []
+    for link in links:
+        # print(link)
+        req = API_Data_Collector(link)
+        compiled_tables.append(req)
+    combined_output = pd.concat(compiled_tables, ignore_index=True)
+    # print(combined_output.to_string())
+    return combined_output
+
+def Community_Cleaner(dataframe):
+    # Clean up the community names
+    patterns = [", Franklin County, Massachusetts",
+                ", Hampden County, Massachusetts",
+                ", Hampshire County, Massachusetts",
+                ", Massachusetts",
+                " town",
+                " city",
+                " Town"]
+    # Create a single regex pattern that matches any of the unwanted substrings
+    combined_pattern = "|".join(map(re.escape, patterns))
+    # Apply the regex substitution to the entire column
+    dataframe["NAME"] = dataframe["NAME"].apply(lambda x: re.sub(combined_pattern, "", x))
+    # To strip any leading/trailing whitespace
+    dataframe["NAME"] = dataframe["NAME"].str.strip()
+    dataframe = dataframe.sort_values(by="NAME")
+    return dataframe
+
+def String_to_Numeric(dataframe):
+    # Change all but the descriptive columns to numeric for calculations
+    dataframe = pd.concat([pd.DataFrame([pd.to_numeric(dataframe[e], errors = 'coerce')
+                            for e in dataframe.columns if e not in
+                            ['GEO_ID','NAME','state','county','county subdivision']]).T,
+                            dataframe[['GEO_ID','NAME','state','county','county subdivision']]], axis = 1)
+    return dataframe
+
+def Table_Math(dataframe):
+    pass
+    print("No math necessary for this table")
+    return dataframe
+
+def Database_Dataframe_Initializer(dataframe, year,year2):
+    # Initialize the Database Dataframe, use NANs for missing values then fill the columns in from B19001
+    ordered_communities = dataframe["NAME"].to_list()
+    # ordered_communities = ['Agawam','Amherst','Ashfield','Belchertown','Bernardston','Blandford','Brimfield','Buckland',
+    #                        'Charlemont','Chester','Chesterfield','Chicopee','Colrain','Conway','Cummington','Deerfield',
+    #                        'East Longmeadow','Easthampton','Erving','Franklin County','Gill','Goshen','Granby','Granville',
+    #                        'Greenfield','Hadley','Hampden','Hampden County','Hampshire County','Hatfield','Hawley','Heath',
+    #                        'Holland','Holyoke','Huntington','Leverett','Leyden','Longmeadow','Ludlow','Massachusetts',
+    #                        'Middlefield','Monroe','Monson','Montague','Montgomery','New Salem','Northampton','Northfield',
+    #                        'Orange','Palmer','Pelham','Plainfield','Rowe','Russell','Shelburne','Shutesbury','South Hadley',
+    #                        'Southampton','Southwick','Springfield','Sunderland','Tolland','Wales','Ware','Warwick',
+    #                        'Wendell','West Springfield','Westfield','Westhampton','Whately','Wilbraham','Williamsburg',
+    #                        'Worthington']
+    Database_df_Headers = ['CEN_SEAVACHU','CEN_VACHU','Per_OWN_OCC','CEN_HUYEARBLT','CEN_HUNOVHCL','CEN_MEDRENT',
+                           'CEN_RENT_INC','CEN_MEDOWNCOSTS','CEN_OWNCOSTS_INC','CEN_MEDOWNVAL','ALL_COSTS30',
+                           'CEN_OCCUPHU','CEN_OWNOCCHU','CEN_RENOCCHU','OWN_COSTS30','RENT_COSTS30','CEN_HOUSINGUNITS',
+                           'HOUS_AFFORD','HAVE_DATA','RECENT_YEAR']
+    # community_series = pd.Series(ordered_communities)
+
+    Database_df = pd.DataFrame(np.nan, index=range(len(ordered_communities)), columns=Database_df_Headers)
+    Database_df.insert(0, "YEAR", year)
+    Database_df.insert(0, "TIME_VALUE", f"{year2}-{year}")
+    Database_df.insert(0, "TIME_TYPE", "5-Year-Estimates")
+    Database_df.insert(0, "COMMUNITY", ordered_communities)
+    Database_df.insert(0, "STATE", "MA")
+    return Database_df
+
+def Dataframe_Allocator(Database_df, B25004, B25035, B25045, B25064, B25071, B25088, B25092, B25097, B25106):
+    # Allocate data to each column as indicated in the data dictionary
+    print(B25106.to_string())
+    Database_df['CEN_SEAVACHU']     = B25004.loc[:, ['B25004_006E']].sum(axis=1)
+    Database_df['CEN_VACHU']        = B25004.loc[:, ['B25004_008E']].sum(axis=1)
+    # Database_df['Per_OWN_OCC']    = B25004.loc[:, ['B25008_002']].sum(axis=1) / B25004.loc[:, ['B25008_001']].sum(axis=1)
+    Database_df['CEN_HUYEARBLT']    = B25035.loc[:, ['B25035_001E']].sum(axis=1)
+    Database_df['CEN_HUNOVHCL']     = B25045.loc[:, ['B25045_003E']].sum(axis=1) / B25045.loc[:, ['B25045_012E']].sum(axis=1)
+    Database_df['CEN_MEDRENT']      = B25064.loc[:, ['B25064_001E']].sum(axis=1)
+
+    Database_df['CEN_RENT_INC']     = B25071.loc[:, ['B25071_001E']].sum(axis=1)
+    Database_df['CEN_MEDOWNCOSTS']  = B25088.loc[:, ['B25088_002E']].sum(axis=1)
+    Database_df['CEN_OWNCOSTS_INC'] = B25092.loc[:, ['B25092_002E']].sum(axis=1)
+    Database_df['CEN_MEDOWNVAL']    = B25097.loc[:, ['B25097_001E']].sum(axis=1)
+    # Database_df['ALL_COSTS30']    = B25106.loc[:, ['B25106_XXXX']].sum(axis=1)
+    # Database_df['CEN_OCCUPHU']    = B25106.loc[:, ['B25106_XXXX']].sum(axis=1)
+    Database_df['CEN_OWNOCCHU']     = B25106.loc[:, ['B25106_002E']].sum(axis=1)
+    Database_df['CEN_RENOCCHU']     = B25106.loc[:, ['B25106_024E']].sum(axis=1)
+
+    Database_df['OWN_COSTS30']      = B25106.loc[:, ['B25106_006E','B25106_010E','B25106_014E','B25106_018E','B25106_022E']].sum(axis=1) / B25106.loc[:, ['B25106_002E']].sum(axis=1)
+    Database_df['RENT_COSTS30']     = B25106.loc[:, ['B25106_028E','B25106_032E','B25106_036E','B25106_040E','B25106_044E']].sum(axis=1) / B25106.loc[:, ['B25106_024E']].sum(axis=1)
+    Database_df['CEN_HOUSINGUNITS'] = B25106.loc[:, ['B25106_001E']].sum(axis=1)
+    Database_df['HOUS_AFFORD']      = B25106.loc[:, ['B25106_001E']].sum(axis=1)
+
+    # Replace large neg values with NAN
+    Database_df.replace(-6666666.660, np.nan, inplace=True)
+    # Show the final database dataframe to make sure it is in good shape
+    # print(Database_df.to_string())
+    return Database_df
+
+def Main(year):
+    start_time = time.time()
+    year = year
+    year2 = year - 4
+    # This variable will contain a list of lists. The first list contains a sequence of levels of geography for each
+    # Given table. B25004 for example has town, county, and state level data. The URLs for this table will be the first
+    # Element in the list. The second element in the list of lists would be the next table wherein that list is contained
+    # Those levels of geography. The intent is to loop through each table and feed it into a function that will instantiate
+    # Each table like B25004 and B25008 into their own dataframes so we can operate on them to create columns and calcs
+    # B25008 is not working skip it for now it only contains 1 column of data
+    tables = ["B25004","B25035","B25045","B25064","B25071","B25088","B25092","B25097","B25106"]
+    # Loop through URLs and use them to build dataframes, then stick them into a list
+    dataframes = []
+    for table in tables:
+        # Gets the api request returns a dataframe with all levels of geography
+        output_table = Table_Concatenator(year, table)
+        dataframes.append(output_table)
+
+    # Run a function that cleans up the community names in the pulled tables
+    # cleaned_tables=[]
+    # for table in dataframes:
+    #     table = Community_Cleaner(table)
+    #     table = String_to_Numeric(table)
+    #     cleaned_tables.append(table)
+    #     # print(table)
+    cleaned_tables = [String_to_Numeric(Community_Cleaner(table)).reset_index(drop=True) for table in dataframes]
+
+
+    # print(dataframes)
+
+    # Assign dataframes to variables for manipulations
+    B25004 = cleaned_tables[0]
+    B25035 = cleaned_tables[1]
+    B25045 = cleaned_tables[2]
+    B25064 = cleaned_tables[3]
+    B25071 = cleaned_tables[4]
+    B25088 = cleaned_tables[5]
+    B25092 = cleaned_tables[6]
+    B25097 = cleaned_tables[7]
+    B25106 = cleaned_tables[8]
+    # Run a function to create the database dataframe
+    print(B25004.to_string())
+    Database_Dataframe = Database_Dataframe_Initializer(B25004, year, year2)
+    print(Database_Dataframe.to_string())
+    Database_Dataframe = Dataframe_Allocator(Database_Dataframe, B25004, B25035, B25045, B25064, B25071, B25088, B25092, B25097, B25106)
+    print(Database_Dataframe.to_string())
+
+    # Run a function to create new columns and do math
+    # Poverty_Dataframe = Table_Math(Poverty_Dataframe)
+
+    # Run a function that will insert data from the original dataframe into the database dataframe, use data dictionary
+    # Database_df = Dataframe_Allocator(Database_df, Poverty_Dataframe)
+    # print(Poverty_Dataframe.to_string())
+    # print(Poverty_Dataframe.head().to_string())
+    # print(Database_df.to_string())
+
+    # Create a CSV file of the dataframe to be uploaded to the database
+    # Database_df.to_csv(
+    #     "C:/Users/jtilsch/OneDrive - Pioneer Valley Planning Commission/Desktop/Projects/Database Design/Data/Census_Poverty_Employment_ Data/Census_Poverty_EmploymentJAXX" + str(year) + ".csv",
+    #     index = False)
+    # print(
+    #     f"Dataframe printed to CSV in \nC:/Users/jtilsch/OneDrive - Pioneer Valley Planning Commission/Desktop/Projects/Database Design/Data/Census_Poverty_Employment_{str(year)}.csv")
+
+
+    end_time = time.time()
+    print(f"Elapsed Runtime: {round(end_time - start_time, 4)} seconds")
+
+
+# Change the year to get a different vintage,
+# If you want one year, comment out the loop and ranged variable years
+# If you want a range of years comment out year and uncomment the years variable and the "for" loop
+#   Keep in mind the range function is [year,year) or Inclusive in the first year, non-inclusive in the second year so
+#   increment 1 more beyond what you want in the latter year
+
+year = 2022
+#
+# years = range(2015,2024)
+# for year in years:
+#     Main(year)
+
+
+
+
+Main(year)
